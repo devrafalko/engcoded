ec68af44f8292f1d7b1effe4ab82578baeae8619
devrafalko <dev.rafalko@gmail.com>
2020-12-27 19:26:30 +0100

    New articles, movies and podcasts added.

--- db/words/collocations.js
+++ db/words/collocations.js
@@ -1,23 +1,4 @@
 import { s } from './../utils/utils';
 
 export default new Map([
-  ['538e53wsjvmtveds', [
-    [`${s('to')} learn`, ['details', 'poznać szczegóły'], ['facts', 'odkryć fakty'], [`${s("sb's")} name`, `poznać ${s('czyjeś')} imię`]],
-    [['person', 'człowiek uczy się'], ['robot', 'robot uczy się'], `learn`],
-  ]],
-  ['538e54zsk1x5068y', [
-    ['versatile', ['person', 'wszechstronna osoba'], ['mind', 'wszechstronny umysł']]
-  ]],
-  ['538e54zsk1x5068z', [
-    ['versatile', ['equipment', 'sprzęt wielofunkcyjny'], ['tool', 'uniwersalne narzędzie']]
-  ]],
-  ['538e51pgk1m7rk0l', [
-    [`${s('to')} discuss`, ['matter', 'omówić sprawę'], ['problem', 'przedyskutować problem']]
-  ]],
-  ['538e53wsjvmtvedt', [
-    [`amazing`, ['person', 'wyjątkowa osoba'], ['musician', 'niezwykły muzyk'], ['player', 'wyjątkowy gracz'], ['offer', 'wyjątkowa oferta'], ['chance', 'wyjątkowa szansa']]
-  ]],
-  ['538e53wsjvmtvec6', [
-    [`${s('to')} experience`, ['emotion', 'doświadczyć uczucia'], ['sensation', 'doznać wrażenia'], ['physical pleasure', 'poczuć fizyczną przyjemność']]
-  ]]
 ]);
