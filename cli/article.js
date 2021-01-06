29550ebbdb878e3d4f0bff884dd2042fb9869078
devrafalko <dev.rafalko@gmail.com>
2021-01-06 18:27:43 +0100

    The CLI article parser has additional header argument

--- cli/article.js
+++ cli/article.js
@@ -4,11 +4,12 @@ const uniq = require('uniqid');
 
 createFile({
   src: process.argv[2],
-  title: process.argv[3] || '',
-  url: process.argv[4] || ''
+  header: process.argv[3] || '',
+  title: process.argv[4] || '',
+  url: process.argv[5] || ''
 });
 
-function createFile({ src, title, url }) {
+function createFile({ src, header, title, url }) {
   if (typeof src !== 'string') return console.error('Error: Incorrect <src> argument. Run:\nnpm run article <src> <title> <url>');
   const directory = path.resolve('./db/articles');
   const fileName = `${src}.js`;
@@ -17,7 +18,7 @@ function createFile({ src, title, url }) {
   fs.stat(filePath, (err, stats) => {
     if (!err) console.error(`The file '${fileName}' already exists or the path is inaccessible.`);
     else {
-      fs.writeFile(filePath, jsTemplate(title, url), 'utf8', (err) => {
+      fs.writeFile(filePath, jsTemplate(header, title, url), 'utf8', (err) => {
         if (err) console.error('Error: The article file could not be created.');
         else console.log('Article template file create.');
       });
@@ -25,12 +26,12 @@ function createFile({ src, title, url }) {
   });
 }
 
-function jsTemplate(title, url) {
+function jsTemplate(header, title, url) {
   return `
 import { w, list, header } from './../utils/utils.js';
 
 export default {
-  title: '',
+  title: '${header}',
   type: 'article',
   id: '${uniq()}',
   thumbnail: '',
